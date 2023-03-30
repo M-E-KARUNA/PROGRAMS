51632abca0df230f2b1ad6d51c2e5968f1af1ef1
KarunaCoder <129355169+KARUNACODER@users.noreply.github.com>
2023-03-30 11:27:25 +0530

    Update cand_elimination.py

--- cand_elimination.py
+++ cand_elimination.py
@@ -1,20 +1,12 @@
 import pandas as pd
 import numpy as np
-#give ur file path
-data=pd.read_csv("/storage/emulated/0/Audio/data.csv") 
-total_attribute=np.array(data.columns)[:-1] 
-data1=data
-data=np.array(data)
-print("dataset\n",data)
-S=np.array([" "]*(len(total_attribute))) 
-G=np.array([["?"]*len(S)],dtype="<U8")
-print("initially \nS={}\nG={}".format(S,G)) 
-count=1
-Gtest=0
-
+all_gen_hyp=[]
+total_attribute=[]
+data1=""
 def getG(g,cur_row) :
     _g=[]
     g1=[]
+    print(g)
     
     for ind,(value,att) in enumerate(zip(g,total_attribute)):
         
@@ -35,7 +27,7 @@ def getG(g,cur_row) :
                 g1[ind]=x
                 
                 _g.append(g1)
-    
+                    
     return np.array(_g)       
             
 def checkG(_G,cur_data) :
@@ -60,7 +52,25 @@ def checkG(_G,cur_data) :
                 
         _G=np.array(_g)
         
+        
+        
     return _G
+
+
+def getHyp(path):
+    global all_gen_hyp,total_attribute,data1
+    data=pd.read_csv(path) 
+    total_attribute=np.array(data.columns)[:-1] 
+    data1=data
+    data=np.array(data)
+    print("dataset\n",data)
+    S=np.array([" "]*(len(total_attribute))) 
+    G=np.array([["?"]*len(S)],dtype="<U8")
+    print("initially \nS={}\nG={}".format(S,G)) 
+    count=1
+    Gtest=0
+
+
        
                     
                 
@@ -75,71 +85,76 @@ def checkG(_G,cur_data) :
 
 
 
-for i,row in enumerate(data):
-    if row[-1]=="Yes":
+    for i,row in enumerate(data):
+        if row[-1]=="Yes":
        
-        _G=[]
-        for g in G:
+            _G=[]
+            for g in G:
           
-            valid=row[:-1]!=g 
+                valid=row[:-1]!=g 
            
-            if np.array_equiv(g[valid],np.array(["?"]*list(valid).count(True))):
+                if np.array_equiv(g[valid],np.array(["?"]*list(valid).count(True))):
                 
-                _G.append(g)
+                    _G.append(g)
                
-        G=_G.copy() 
-        if count:
-            count=0
-            S=row[:-1].copy() 
-            continue
-        differ=(S!=row[:-1]) 
-        S[differ]="?"  
+            G=_G.copy() 
+            if count:
+                count=0
+                S=row[:-1].copy() 
+                continue
+            differ=(S!=row[:-1]) 
+            S[differ]="?"  
              
       
         
         
-        pass
+       
         
         
-    else:
-        """differ=(S!=row[:-1])
+        else:
+            """differ=(S!=row[:-1])
         if not np.array_equiv(S[differ],np.array(["?"]*list(differ).count(True))):
                  pass
                  #do"""
-        _G=[]
-        for g in G:
-            differ=(g!=row[:-1]) 
-            if np.array_equiv(g[differ],np.array(["?"]*list(differ).count(True))):
-                Gtest=1
-                _G.extend(getG(g,row[:-1]) ) 
+            _G=[]
+            for g in G:
+                differ=(g!=row[:-1]) 
+                if np.array_equiv(g[differ],np.array(["?"]*list(differ).count(True))):
+                    Gtest=1
+                    _G.extend(getG(g,row[:-1]) ) 
                 
                 
-            else:
-                _G.append(g) 
-        _G=np.array(_G)
+                else:
+                    _G.append(g) 
+            _G=np.array(_G)
         
-        if Gtest:
-            Gtest=0
-           # print (G)
-            G=checkG(_G,data[:i+1])
+            if Gtest:
+                Gtest=0
+           
+                G=checkG(_G,data[:i+1])
+                
             
 #print(S)  
 #print(np.array(G))
-G=np.array(G)
+    G=np.array(G)
 
-print("before comparison\nS={}\nG={}".format(S,G))
-all_gen_hyp=[]
-for g in G:
-    for i in range(len(S)) :
-        if g[i]=="?" and S[i]!="?":
-             _g=g.copy() 
-             _g[i]=S[i]
+    print("before comparison\nS={}\nG={}".format(S,G))
+    
+    for g in G:
+        for i in range(len(S)) :
+            if g[i]=="?" and S[i]!="?":
+                 _g=g.copy() 
+                 _g[i]=S[i]
            
-             all_gen_hyp.append(tuple(_g))
-if len(all_gen_hyp)==0:
-    all_gen_hyp=G
-else:
-    all_gen_hyp=np.array(list(set(all_gen_hyp)) ) 
-print("All Possible Hypothesis",*all_gen_hyp,sep="\n")
+                 all_gen_hyp.append(tuple(_g))
+    if len(all_gen_hyp)==0:
+        all_gen_hyp=G
+    else:
+        all_gen_hyp=np.array(list(set(all_gen_hyp)) ) 
+    print("All Possible Hypothesis",*all_gen_hyp,sep="\n")
+
+
+
+if __name__=="__main__":
+    getHyp(input("enter the path:")) 
  
-       
